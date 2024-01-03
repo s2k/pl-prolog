9767bb14f33d40169f835041abaaebed695bf224
Stephan Kämper <the.tester@seasidetesting.com>
2024-01-03 11:35:35 +0100

    minor change

--- bigger.pl
+++ bigger.pl
@@ -9,4 +9,15 @@ bigger_than(A, B) :-
 
 bigger_than(A, B) :-
   bigger(A, C),
+  write(C),
+  
   bigger_than(C, B).
+
+much_bigger_than(A, B) :-
+  bigger_than(A, X),
+  bigger_than(X, Y),
+  bigger_than(Y, B).
+
+in_between(BIG, SMALLER, MIDDLE) :-
+  bigger_than(BIG,MIDDLE),
+  bigger_than(MIDDLE,SMALLER).
